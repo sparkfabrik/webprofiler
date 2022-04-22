9293e15c95c15bd237d57126e57cfcd29969f7b7
Luca Lusso <luca.lusso@sparkfabrik.com>
2022-04-22 11:55:04 +0200

    Refactor database panel javascript

---
+++ js/dashboard.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Main dashboard script.
+ */
+(function (Drupal) {
+
+  "use strict";
+
+  Drupal.behaviors.webprofiler_dashboard = {
+    attach: function (context) {
+    }
+  }
+})(Drupal);
--- js/database.js
+++ js/database.js
@@ -2,39 +2,58 @@
  * @file
  * Database panel app.
  */
-(function ($, Drupal, drupalSettings) {
+(function (Drupal) {
 
   "use strict";
 
   Drupal.behaviors.webprofiler_database = {
     attach: function (context) {
       // Swap placeholders.
-      $('.js--executable-toggle').once().click(function (e) {
-        let qid = e.target.dataset.webprofilerQid;
-        $('.js--placeholder-query-' + qid).toggleClass('is-hidden');
-        $('.js--executable-query-' + qid).toggleClass('is-hidden');
+      once('executable-toggle', '[data-webprofiler-executable-toggle]', context).forEach(function (element) {
+        element.addEventListener('click', function (e) {
+          let qid = e.target.dataset.webprofilerQid;
+          document.querySelector("[data-webprofiler-placeholder-query='"+qid+"']").classList.toggle('is-hidden');
+          document.querySelector("[data-webprofiler-executable-query='"+qid+"']").classList.toggle('is-hidden');
+        });
       });
 
       // Show placeholders.
-      $('.js--placeholder-toggle').once().click(function (e) {
-        let qid = e.target.dataset.webprofilerQid;
-        $('.js--placeholder-target-' + qid).toggleClass('is-hidden');
+      once('placeholder-toggle', '[data-webprofiler-placeholder-toggle]', context).forEach(function (element) {
+        element.addEventListener('click', function (e) {
+          let qid = e.target.dataset.webprofilerQid;
+          document.querySelector("[data-webprofiler-placeholder-target='"+qid+"']").classList.toggle('is-hidden');
+        });
       });
 
       // Copy to clipboard.
-      $('.js--query-copy').once().click(function (e) {
-        let qid = e.target.dataset.webprofilerQid;
-        let query = $('.js--executable-query-' + qid)[0].innerText;
-        navigator.clipboard.writeText(query);
+      once('query-copy', '[data-webprofiler-copy]', context).forEach(function (element) {
+        element.addEventListener('click', function (e) {
+          let qid = e.target.dataset.webprofilerQid;
+          let query = document.querySelector("[data-webprofiler-executable-query='"+qid+"']").innerText;
+          navigator.clipboard.writeText(query);
+        });
       });
 
-      // Highlight queries.
-      if (typeof hljs != "undefined") {
-        hljs.configure({
-          ignoreUnescapedHTML: true
+      // Dynamically download highlightjs.
+      once('database', '.webprofiler__panel').forEach(function (element) {
+        loadjs(
+          [
+            'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/highlight.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/default.min.css'
+          ],
+          'highlight'
+        );
+
+        loadjs.ready('highlight', function() {
+          // Highlight queries.
+          if (typeof hljs != "undefined") {
+            hljs.configure({
+              ignoreUnescapedHTML: true
+            });
+            hljs.highlightAll();
+          }
         });
-        hljs.highlightAll();
-      }
+      });
     }
   }
-})(jQuery, Drupal, drupalSettings);
+})(Drupal);
