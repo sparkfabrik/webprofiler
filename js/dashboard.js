9fa4775947b554ec9fd6b3cd58ee06704c3fa3f9
Luca Lusso <luca.lusso@sparkfabrik.com>
2022-06-30 09:04:12 +0200

    Automatically open selected panel

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,12 +2,22 @@
  * @file
  * Main dashboard script.
  */
-(function (Drupal) {
+(function (Drupal, drupalSettings) {
 
   "use strict";
 
   Drupal.behaviors.webprofiler_dashboard = {
     attach: function (context) {
+      // Automatically open the panel if the URL contains the query parameter.
+      once('opener', '.webprofiler__collectors', context).forEach(function (element) {
+        const path = drupalSettings.path;
+
+        if (path.currentQuery && 'panel' in path.currentQuery) {
+          const panel = path.currentQuery['panel'];
+          const panel_link = document.querySelector(".webprofiler__collectors [data-collector-name='" + panel + "']");
+          panel_link.click();
+        }
+      });
     }
   }
-})(Drupal);
+})(Drupal, drupalSettings);
