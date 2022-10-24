871e4bf0228873fb0fd366ca8741318ceda14abd
Luca Lusso <luca.lusso@sparkfabrik.com>
2022-10-24 10:14:39 +0200

    Speedup database javascript

--- js/database.js
+++ js/database.js
@@ -6,40 +6,61 @@
 
   "use strict";
 
+  const queryTpl = _.template(`
+    <div>
+        <dl>
+            <dt>Time</dt>
+            <dd><% print(time); %> ms</dd>
+            <dt>Caller</dt>
+            <dd><% print(caller); %></dd>
+            <dt>Database</dt>
+            <dd><% print(db); %></dd>
+            <dt>Target</dt>
+            <dd><% print(target); %></dd>
+        </dl>
+    </div>
+
+    <% if (hasArgs == 1) {%><a class="wp-executable-toggle">Swap placeholders</a><%}%>
+    <a class="wp-query-copy">Copy query</a>
+    <% if (type == "SELECT") {%><a href="<% print(explainPath); %>" class="use-ajax">Explain</a><%}%>
+
+    <div class="js--explain-target-<% print(qid); %>"></div>
+  `);
+
   Drupal.behaviors.webprofiler_database = {
     attach: function (context) {
-      // Swap placeholders.
-      once('executable-toggle', '[data-webprofiler-executable-toggle]', context).forEach(function (element) {
-        element.addEventListener('click', function (e) {
-          let qid = e.target.dataset.webprofilerQid;
-          document.querySelector("[data-webprofiler-placeholder-query='"+qid+"']").classList.toggle('is-hidden');
-          document.querySelector("[data-webprofiler-executable-query='"+qid+"']").classList.toggle('is-hidden');
-        });
-      });
+      once('db', '.wp-db-query').forEach(function (element) {
+        let result =
+          queryTpl({
+            'time': element.dataset.wpTime,
+            'caller': element.dataset.wpClass,
+            'db': element.dataset.wpDb,
+            'target': element.dataset.wpTarget,
+            'hasArgs': element.dataset.wpHasArgs,
+            'type': element.dataset.wpType,
+            'qid': element.dataset.wpQid,
+            'explainPath': element.dataset.wpExplainPath
+          });
 
-      // Show placeholders.
-      once('placeholder-toggle', '[data-webprofiler-placeholder-toggle]', context).forEach(function (element) {
-        element.addEventListener('click', function (e) {
-          let qid = e.target.dataset.webprofilerQid;
-          document.querySelector("[data-webprofiler-placeholder-target='"+qid+"']").classList.toggle('is-hidden');
-        });
-      });
+        element.innerHTML += result;
 
-      // Copy to clipboard.
-      if (navigator.clipboard && window.isSecureContext) {
-        once('query-copy', '[data-webprofiler-copy]', context).forEach(function (element) {
-          element.addEventListener('click', function (e) {
-            let qid = e.target.dataset.webprofilerQid;
-            let query = document.querySelector("[data-webprofiler-executable-query='" + qid + "']").innerText;
+        // Swap placeholders.
+        element.querySelector('.wp-executable-toggle').addEventListener('click', function (e) {
+          element.querySelector('.wp-query-placeholder').classList.toggle('is-hidden');
+          element.querySelector('.wp-query-executable').classList.toggle('is-hidden');
+        });
+
+        // Copy to clipboard.
+        if (navigator.clipboard && window.isSecureContext) {
+          element.querySelector('.wp-query-copy').addEventListener('click', function (e) {
+            let query = element.querySelector('.wp-query-executable').innerText;
             navigator.clipboard.writeText(query);
           });
-        });
-      }
-      else {
-        once('query-copy', '[data-webprofiler-copy]', context).forEach(function (element) {
-          element.classList.toggle('is-hidden');
-        });
-      }
+        }
+        else {
+          element.querySelector('.wp-query-copy').classList.toggle('is-hidden');
+        }
+      });
     }
   }
 })(Drupal);
