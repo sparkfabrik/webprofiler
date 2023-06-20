00dd0a9551e17d2fb08379bdc4e18459ceba2e35
Luca Lusso <lussoluca@gmail.com>
2023-06-20 17:37:29 +0200

    #71 Add an installation test

---
+++ tests/src/Functional/WebprofilerFunctionalTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\Tests\webprofiler\Functional;
+
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Functional tests for the WebProfiler module.
+ *
+ * @group webprofiler
+ */
+class WebprofilerFunctionalTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'node',
+    'test_page_test',
+    'webprofiler',
+  ];
+
+  /**
+   * A user with authenticated permissions.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $user;
+
+  /**
+   * A user with admin permissions.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $adminUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->config('system.site')->set('page.front', '/test-page')->save();
+    $this->user = $this->drupalCreateUser();
+    $this->adminUser = $this->drupalCreateUser();
+    $this->adminUser->addRole($this->createAdminRole('admin', 'admin'));
+    $this->adminUser->save();
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests if installing the module, won't break the site.
+   */
+  public function testInstallation() {
+    $session = $this->assertSession();
+    $this->drupalGet('<front>');
+    // Ensure the status code is success:
+    $session->statusCodeEquals(200);
+    // Ensure the correct test page is loaded as front page:
+    $session->pageTextContains('Test page text.');
+  }
+
+  /**
+   * Tests if uninstalling the module, won't break the site.
+   */
+  public function testUninstallation() {
+    // Go to uninstallation page an uninstall WebProfiler:
+    $session = $this->assertSession();
+    $page = $this->getSession()->getPage();
+    $this->drupalGet('/admin/modules/uninstall');
+    $session->statusCodeEquals(200);
+    $page->checkField('edit-uninstall-webprofiler');
+    $page->pressButton('edit-submit');
+    $session->statusCodeEquals(200);
+    // Confirm uninstall:
+    $page->pressButton('edit-submit');
+    $session->statusCodeEquals(200);
+    $session->pageTextContains('The selected modules have been uninstalled.');
+    // Retest the frontpage:
+    $this->drupalGet('<front>');
+    // Ensure the status code is success:
+    $session->statusCodeEquals(200);
+    // Ensure the correct test page is loaded as front page:
+    $session->pageTextContains('Test page text.');
+  }
+
+}
