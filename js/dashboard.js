48ee60b5ee1d6fefa7f6375aae77ec87ce5d2718
Luca Lusso <luca.lusso@sparkfabrik.com>
2023-10-28 00:43:57 +0200

    Fix Eslint

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,18 +6,16 @@
   Drupal.behaviors.webprofiler_dashboard = {
     attach(context) {
       // Automatically open the panel if the URL contains the query parameter.
-      once("opener", ".webprofiler__collectors", context).forEach(function (
-        element
-      ) {
+      once('opener', '.webprofiler__collectors', context).forEach(function () {
         const { path } = drupalSettings;
 
-        if (path.currentQuery && "panel" in path.currentQuery) {
+        if (path.currentQuery && 'panel' in path.currentQuery) {
           const { panel } = path.currentQuery;
           const panel_link = document.querySelector(
             `.webprofiler__collectors [data-collector-name='${panel}']`
           );
           panel_link.click();
-          panel_link.parentNode.className += " active";
+          panel_link.parentNode.className += ' active';
         }
       });
     },
