06d933653862e3941524e5ca552f6724120ef282
Luca Lusso <luca.lusso@sparkfabrik.com>
2024-02-08 12:53:21 +0100

    Fix an issue with ThemeDataCollector. Fix a performance issue on sites with a lot of routes

--- src/DataCollector/ThemeDataCollector.php
+++ src/DataCollector/ThemeDataCollector.php
@@ -117,9 +117,13 @@ class ThemeDataCollector extends DataCollector implements HasPanelInterface, Lat
     ];
 
     if ($this->themeNegotiator instanceof ThemeNegotiatorWrapper) {
-      $this->data['negotiator'] = [
-        'class' => $this->getMethodData($this->themeNegotiator->getNegotiator(), 'determineActiveTheme'),
-      ];
+      $theme_negotiator = $this->themeNegotiator->getNegotiator();
+
+      if ($theme_negotiator != NULL) {
+        $this->data['negotiator'] = [
+          'class' => $this->getMethodData($theme_negotiator, 'determineActiveTheme'),
+        ];
+      }
     }
   }
 
--- src/Theme/ThemeNegotiatorWrapper.php
+++ src/Theme/ThemeNegotiatorWrapper.php
@@ -44,11 +44,11 @@ class ThemeNegotiatorWrapper extends ThemeNegotiator {
   /**
    * Return the current theme negotiator.
    *
-   * @return \Drupal\Core\Theme\ThemeNegotiatorInterface
+   * @return \Drupal\Core\Theme\ThemeNegotiatorInterface|null
    *   The current theme negotiator.
    */
-  public function getNegotiator(): ThemeNegotiatorInterface {
-    return $this->negotiator;
+  public function getNegotiator(): ?ThemeNegotiatorInterface {
+    return $this->negotiator ?? NULL;
   }
 
 }
