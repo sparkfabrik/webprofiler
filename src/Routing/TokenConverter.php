8151f75792e09a47afad75dbdd9bf2000d34a64c
Luca Lusso <lussoluca@gmail.com>
2024-04-11 10:37:47 +0200

    Issue #85 by lussoluca: Got that PHP fatal error when running drush cr command in the project

--- src/Routing/TokenConverter.php
+++ src/Routing/TokenConverter.php
@@ -41,7 +41,11 @@ class TokenConverter implements ParamConverterInterface {
    * {@inheritdoc}
    */
   public function applies($definition, $name, Route $route): bool {
-    if (array_key_exists('type', $definition) && $definition['type'] === 'webprofiler:token') {
+    if (
+      is_array($definition) &&
+      array_key_exists('type', $definition) &&
+      $definition['type'] === 'webprofiler:token'
+    ) {
       return TRUE;
     }
 
